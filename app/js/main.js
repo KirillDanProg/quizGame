4ff7bedfd5d4bb0305f5f99e6228b6697858a3fc
KirillVN <kirill137777@gmail.com>
2022-05-17 10:01:02 +0300

    right answer added

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,17 @@ let count = 100;
 let score = 0;
 let timer = true;
 let limit = null;
-let correctAnswer = ""
+let correctAnswer = "";
+let arrNum = [];
+let i = 0;
+let intervalID = null
+
+start.addEventListener("click", getQuiz);
+// box.addEventListener("click", checkAnswer);
 
 function getQuiz() {
   limit++;
+  
   if (limit <= 10) {
     fetch(urlQuiz)
       .then((resp) => resp.json())
@@ -21,24 +28,29 @@ function getQuiz() {
       )
       .then(() => {
         scale.style.display = "block";
+  console.log(correctAnswer)
+
         scaleStart();
+        box.addEventListener("click", checkAnswer);
       });
     scale.style.backgroundColor = "green";
   } else {
-    scale.style.display = "none";
-    box.innerHTML = `Your score: ${score}`;
-    let tryAgain = document.createElement("div");
-    tryAgain.innerHTML = "try again";
-    tryAgain.classList.add("try-again");
-    box.appendChild(tryAgain);
-    tryAgain.addEventListener("click", getQuiz);
-    limit = 0;
-    score = 0;
+    showResult();
   }
 }
 
-let arrNum = [];
-let i = 0;
+function showResult() {
+  scale.style.display = "none";
+  box.innerHTML = `Your score: ${score}`;
+  let tryAgain = document.createElement("div");
+  tryAgain.innerHTML = "try again";
+  tryAgain.classList.add("try-again");
+  box.appendChild(tryAgain);
+  tryAgain.addEventListener("click", getQuiz);
+  limit = 0;
+  score = 0;
+}
+
 function getNum() {
   while (arrNum.length < 4) {
     let num = Math.floor(Math.random() * 4);
@@ -51,7 +63,10 @@ function getNum() {
 }
 
 function nextQn(data) {
-   correctAnswer = data.correctAnswer;
+  box.innerHTML = "";
+
+
+  correctAnswer = data.correctAnswer;
   let incorrectAnswers = data.incorrectAnswers;
   let optionList = incorrectAnswers;
   optionList.splice(
@@ -60,9 +75,6 @@ function nextQn(data) {
     correctAnswer
   );
 
-  box.innerHTML = "";
-  
- 
   return `
   <div class="question">${data.question}</div>
    ${optionList.map((option) => `<div class="answer">${option}</div>`).join("")}
@@ -70,22 +82,38 @@ function nextQn(data) {
 }
 
 function checkAnswer(e) {
+
   let target = e.target;
-  if (target.innerHTML !== correctAnswer && target.classList.contains("answer")) {
+  if (
+    target.innerHTML !== correctAnswer &&
+    target.classList.contains("answer")
+  ) {
     target.style.backgroundColor = "red";
-    timer = false;
-    getQuiz();
-  } else if (target.innerHTML === correctAnswer && target.classList.contains("answer")) {
+    showCorrectAnswer()
+  } else if (
+    target.innerHTML === correctAnswer &&
+    target.classList.contains("answer")
+  ) {
     target.style.backgroundColor = "green";
     score++;
-    timer = false;
+    clearInterval(intervalID)
     getQuiz();
   }
 }
 
+function showCorrectAnswer() {
+  clearInterval(intervalID);
+  box.removeEventListener("click", checkAnswer);
+
+  box.innerHTML += `
+  <div class="correct-answer">The answer is: ${correctAnswer}</div>
+  `
+  setTimeout(() => getQuiz(), 5000)
+}
+
 function scaleStart() {
   count = 100;
-  let intervalID = setInterval(() => {
+  intervalID = setInterval(() => {
     count -= 0.1;
     if (count < 50) {
       scale.style.backgroundColor = "yellow";
@@ -106,5 +134,5 @@ function scaleStart() {
   scale.style.backgroundColor = "green";
   scale.classList.add("scale-active");
 }
-start.addEventListener("click", getQuiz);
-box.addEventListener("click", checkAnswer);
+
+
